88192192ca359d239373ff37ca54cefc4679a15c
ZoranUTF8 <janjiczoran23@gmail.com>
2022-11-08 20:45:48 +0100

    add pagination backend

--- controllers/employees.js
+++ controllers/employees.js
@@ -5,13 +5,33 @@ const EmployeeModel = require("../models/Employee");
 
 const getEmployees = async (req, res) => {
   // We get allEmployees created by all admins, if we want to show only the one that are created by the logged in admin we can filter them here
-  const allEmployees = await EmployeeModel.find({}).sort("createdAt");
+
+  // Get all employees
+  const queryObject = {};
+  let result = EmployeeModel.find(queryObject);
+  // Sort byt newest first
+  result.sort("-createdAt");
+
+  // Pagination
+  const page = Number(req.query.page || 1);
+  const limit = Number(req.query.limit || 10);
+  const skip = (page - 1) * limit;
+
+  result = result.skip(skip).limit(limit);
+
+  const allEmployees = await result;
 
   if (!allEmployees) {
     throw new NotFoundError(`No employees found.`);
   }
 
-  res.status(StatusCodes.OK).json({ count: allEmployees.length, allEmployees });
+  // Get total number of employees and number of pages
+  const totalJobs = await EmployeeModel.countDocuments(queryObject);
+  const numOfPages = Math.ceil(totalJobs / limit);
+
+  res
+    .status(StatusCodes.OK)
+    .json({ count: totalJobs, numOfPages, allEmployees });
 };
 
 const getEmployee = async (req, res) => {
